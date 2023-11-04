d3494f0d7a1157c3b8d95564e0c530ebd0d8d9e6
Sergeev Mikhail <sergeev-michael@mail.ru>
2023-11-04 18:43:15 +0600

    add onClick on Delete button

--- src/Orders/Orders.tsx
+++ src/Orders/Orders.tsx
@@ -2,13 +2,13 @@ import {Order} from "../types";
 import React from "react";
 import './Orders.css';
 
-const Orders:React.FC<Order>= ({name,count,price,image}) => {
+const Orders:React.FC<Order>= ({name,count,price,image,onClickDelete}) => {
     return (
         <div className="orders-box">
             <strong>{name}</strong>
             <span>x {count}</span>
             <strong>{price}</strong>
-            <button type="button" className="delete-btn">
+            <button type="button" className="delete-btn" onClick={onClickDelete}>
                 <img src={image} alt={name} className="delete-img"/>
             </button>
         </div>
